36d522e3079a0da181b2c40d51f7b307a24534d2
catlvov <kostalvov390@gmail.com>
2025-12-09 20:21:33 +0800

    Add project files.

---
+++ Academy/Human.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+    internal class Human
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+
+        public Human(string lastName,string firstName, int age)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            Age = age;
+            Console.WriteLine($"HCostructor: {GetHashCode()}");
+        }
+
+        ~Human()
+        {
+            Console.WriteLine($"HDestructor: {GetHashCode()}");
+        }
+        public void info()
+        {
+            Console.WriteLine($"LastName:{LastName}\tFirstName:{FirstName}\tAge:{Age}");
+        }
+    }
+}
---
+++ Academy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human("BlackMoore", "Richie", 32);
+            human.info();
+        }
+    }
+}
